5422e14556e6041c42ecd95de886fd42a8877360
warbee0712 <warbee0712@outlook.com>
2017-01-21 10:12:38 -0200

    Add timers state to TimersDashboard

--- src/EditableTimerList.js
+++ src/EditableTimerList.js
@@ -2,22 +2,14 @@ import React from 'react'
 
 import EditableTimer from './EditableTimer'
 
-const EditableTimerList = () => (
+const EditableTimerList = props => (
   <div>
-    <EditableTimer
-      title='Learn React'
-      project='Web Domination'
-      elapsed='8986300'
-      runingSince={null}
-      editFormOpen={false}
-    />
-    <EditableTimer
-      title='Learn extreme ironing'
-      project='World Domination'
-      elapsed='3890985'
-      runingSince={null}
-      editFormOpen={true}
-    />
+    {props.timers.map(timer => (
+      <EditableTimer
+        key={timer.id}
+        {...timer}
+      />
+    ))}
   </div>
 )
 
--- src/TimersDashboard.js
+++ src/TimersDashboard.js
@@ -1,18 +1,47 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
+import uuid from 'uuid'
 
 import EditableTimerList from './EditableTimerList'
 import ToggleableTimerForm from './ToggleableTimerForm'
 
-const TimerDashboard = () => (
-  <Grid columns='3' centered>
-    <Grid.Column>
-      <EditableTimerList />
-      <ToggleableTimerForm
-        isOpen={true}
-      />
-    </Grid.Column>
-  </Grid>
-)
+class TimerDashboard extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      timers: [
+        {
+          title: 'Practice Tennis',
+          project: 'Gym Chores',
+          id: uuid.v4(),
+          elapsed: 5456099,
+          runningSince: Date.now()
+        },
+        {
+          title: 'Bake Cake',
+          project: 'Kitchen Chores',
+          id: uuid.v4(),
+          elapsed: 1273998,
+          runningSince: null
+        }
+      ]
+    }
+  }
+  render () {
+    return (
+      <Grid columns='3' centered>
+        <Grid.Column>
+          <EditableTimerList
+            timers={this.state.timers}
+          />
+          <ToggleableTimerForm
+            isOpen={true}
+          />
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
 
 export default TimerDashboard
