3061882259cf9083c7854062e7652d8f6c9dadee
warbee0712 <warbee0712@outlook.com>
2017-01-21 10:23:17 -0200

    Add state to ToggleableTimerForm

--- src/ToggleableTimerForm.js
+++ src/ToggleableTimerForm.js
@@ -3,18 +3,31 @@ import { Segment, Button } from 'semantic-ui-react'
 
 import TimerForm from './TimerForm'
 
-const ToggleableTimerForm = props => {
-  if (props.isOpen) {
-    return <TimerForm />
-  } else {
-    return (
-      <Segment basic textAlign='center'>
-        <Button
-          basic
-          icon='plus icon'
-        />
-      </Segment>
-    )
+class ToggleableTimerForm extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      isOpen: false
+    }
+  }
+  handleFormOpen () {
+    this.setState({ isOpen: true })
+  }
+  render () {
+    if (this.state.isOpen) {
+      return <TimerForm />
+    } else {
+      return (
+        <Segment basic textAlign='center'>
+          <Button
+            basic
+            icon='plus'
+            onClick={this.handleFormOpen.bind(this)}
+          />
+        </Segment>
+      )
+    }
   }
 }
 
