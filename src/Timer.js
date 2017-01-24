cfb965ed69f7b08622e11b6619ea3979d39b0163
warbee0712 <warbee0712@outlook.com>
2017-01-24 20:11:17 -0200

    Add Timer Control Buttons animation

--- src/Timer.js
+++ src/Timer.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import { Card, Header, Icon } from 'semantic-ui-react'
+import { Card, Header } from 'semantic-ui-react'
 import { renderElapsedString } from './helpers'
 
 import TimerActionButton from './TimerActionButton'
+import TimerControlButtons from './TimerControlButtons'
 
 class Timer extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      showButtons: false
+    }
+  }
   componentDidMount () {
     this.forceUpdateInterval = setInterval(() => this.forceUpdate(), 50)
   }
   componentWillUnmount () {
     clearInterval(this.forceUpdateInterval)
+    clearTimeout(this.showButtonsTimeout)
+  }
+  showButtons () {
+    this.setState({ showButtons: true })
+  }
+  hideButtons () {
+    this.setState({ showButtons: false })
   }
   render () {
     const elapsedString = renderElapsedString(
@@ -17,7 +32,13 @@ class Timer extends React.Component {
       this.props.runningSince
     )
     return (
-      <Card centered>
+      <Card
+        centered
+        onMouseEnter={() => this.showButtonsTimeout = setInterval(() => this.showButtons(), 500)}
+        onMouseLeave={() => {
+          clearTimeout(this.showButtonsTimeout)
+          this.hideButtons()
+        }}>
         <Card.Content>
           <Card.Header>
             {this.props.title}
@@ -28,22 +49,12 @@ class Timer extends React.Component {
           <Card.Description>
             <Header as='h2' textAlign='center'>{ elapsedString }</Header>
           </Card.Description>
-          <Card.Content extra>
-            <span className="right floated">
-              <Icon
-                name='edit'
-                onClick={this.props.onEditClick}
-                style={{ cursor: 'pointer' }}
-              />
-            </span>
-            <span className="right floated">
-              <Icon
-                name='trash'
-                style={{ cursor: 'pointer' }}
-                onClick={() => this.props.onTrashClick(this.props.id)}
-              />
-            </span>
-          </Card.Content>
+          <TimerControlButtons
+            id={this.props.id}
+            showButtons={this.state.showButtons}
+            onTrashClick={this.props.onTrashClick}
+            onEditClick={this.props.onEditClick}
+          />
         </Card.Content>
         <TimerActionButton
           timerIsRunning={!!this.props.runningSince}
